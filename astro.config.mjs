382702f67f21ab1bcb0e0fec07237571d56f9c29
alexxgraham <destgraham@icloud.com>
2025-02-09 15:59:38 -0800

    Initialize repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
