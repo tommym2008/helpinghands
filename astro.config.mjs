4eceb751a67a1a6d5fd2f0fbc2b0d53a903e2056
alexxgraham <destgraham@icloud.com>
2025-02-22 13:27:28 -0800

    Applied primary site styes and logos + some lightweight auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  experimental: {
+    svg: true
+  },
   devToolbar: {
     enabled: false
   },
